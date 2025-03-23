3665be0128638c22571e6f00c0d26944f9ea7a33
Pranav Pradeep <pranavpradeep05@gmail.com>
2025-03-23 15:27:31 +0530

    made changes to sidebar.jsx

--- clientside/src/components/Sidebar.jsx
+++ clientside/src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ export default function Sidebar(){
   const {userState}=useContext(AuthContext)
   const [showOnlineOnly,setShowOnlineOnly]=useState(false)
   console.log("Chat state in Sidebar ",chatState)
-  const filteredUsers=showOnlineOnly? chatState.users.filter(user=>userState.onlineUsers.includes(user._id)):chatState.users;
+  const filteredUsers=showOnlineOnly? chatState.users.filter(user=>userState.onlineUsers?.includes(user._id)):chatState.users;
   return (
     <aside className="h-full w-20 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200">
         <div className="border-b border-base-300 w-full p-5">
@@ -39,7 +39,7 @@ export default function Sidebar(){
                                 className="size-12 object-cover rounded-full"
                             />
                             
-                            {userState.onlineUsers.includes(user._id) && (
+                            {userState.onlineUsers?.includes(user._id) && (
                                 <span
                                 className="absolute bottom-0 right-0 size-3 bg-green-500 
                                 rounded-full ring-2 ring-zinc-900"
